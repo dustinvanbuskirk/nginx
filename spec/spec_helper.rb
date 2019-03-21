2ba174e4f3ee9f29cf4048c07e69005920ed90cb
Dustin Van Buskirk <dev@vanbuskirk.me>
2019-03-21 14:18:17 -0700

    adding allure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+require 'allure-rspec'
 
 RSpec.configure do |config|
   config.color = true               # Use color in STDOUT
@@ -9,5 +10,12 @@ RSpec.configure do |config|
   # run all specs when using a filter, but no spec match
   config.run_all_when_everything_filtered = true
 
+  config.include AllureRSpec::Adaptor # Export Allure Reports
+
   Ohai::Config[:log_level] = :error
+end
+
+
+AllureRSpec.configure do |c|
+    c.output_dir = "/codefresh/volume/allure-results"
 end
